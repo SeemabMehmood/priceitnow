e0fd271deeca294a04543ad9d17f5b88aa5a1731
SeemabMehmood <seemab.mehmood25@gmail.com>
2020-08-09 17:41:39 +0500

    Added scraper for yoogiscloset website

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -89,6 +89,49 @@ class Scraper < ApplicationRecord
     end
   end
 
+  def self.scrape_from_yoogiscloset
+    uri = 'https://www.yoogiscloset.com/handbags'
+    html = open(uri)
+    doc  = Nokogiri::HTML(html)
+    all_handbags = doc.css('.products-grid').css('.item')
+    page_num = 1
+
+    while all_handbags.present?
+      all_handbags.each do |bag_div|
+        website = bag_div.css('.product-image-wrapper').css('a').attribute('href').value
+        name = bag_div.css('.item-main-info').css('.product-name').css('a').attribute('title').value
+        image = bag_div.css('.product-image-wrapper').css('.primary_image').attribute('src').value
+        brand = bag_div.css('.item-main-info').css('.designer-name').text
+        condition = bag_div.css('.product-list-attributes').css('.product-condition').children.children.text
+        product_id = bag_div.css('.item-main-info').css('.fav-outer-container').css('.favorites-toggle').attribute('data-wishlist-product-id').value
+
+        price = bag_div.css('.price-box').css('.special-price').css('.price')
+        if price.length > 1
+          new_price = price[0].text.gsub(',', '').gsub(/\s+/, "")
+          previous_price = price[1].text.gsub(',', '').gsub(/\s+/, "")
+        else
+          new_price = price.text.gsub(',', '').gsub(/\s+/, "")
+        end
+
+        persisted_handbag = Handbag.find_by(product_id: product_id)
+
+        if persisted_handbag.present?
+          next if persisted_handbag.prices.latest.price == new_price[1..-1]
+          persisted_handbag.prices.create(price: new_price[1..-1], currency: new_price[0])
+        else
+          handbag = Handbag.create(name: name, website: website, condition: condition, product_id: product_id, brand: brand)
+          if previous_price.present?
+            handbag.prices.create(price: previous_price[1..-1], currency: previous_price[0])
+          end
+          handbag.prices.create(price: new_price[1..-1], currency: new_price[0])
+        end
+      end
+      page_num += 1
+      doc  = Nokogiri::HTML(open("#{uri}?p=#{page_num}"))
+      all_handbags = doc.css('.products-grid').css('.item')
+    end
+  end
+
   def self.scrape_prices_data
     scrape_collector_square_price_data
     scrape_from_shop_rebag
