5b28e026bbda89b9d26c9db2d7388c622a5d6e29
SeemabMehmood <seemab.mehmood25@gmail.com>
2020-08-14 23:46:13 +0500

    Adding scheduling for vestiaire collective

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,4 +2,8 @@ every 1.day, at: '12:00 AM' do
   runner 'Scraper.scrape_prices_data'
 end
 
+every 1.day, at: '03:00 AM' do
+  runner 'Scraper.scrape_from_vestiairecollective'
+end
+
 set :output, "log/cron.log"
